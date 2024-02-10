89cbfb6b59ecd6e3e5bfde0681a9b00fa82ea871
AndreyKondakovGW <andreykondakov2302@gmail.com>
2024-02-10 15:44:14 +0300

    add detection model

---
+++ src/utils.py
@@ -0,0 +1,9 @@
+import cv2
+import base64
+
+
+def convert_image_to_base64(image_np):
+    _, buffer = cv2.imencode('.jpg', image_np)
+    jpg_as_text = base64.b64encode(buffer)
+    base64_string = jpg_as_text.decode('utf-8')
+    return base64_string
